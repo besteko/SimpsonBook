0ab9091a0caf0cbd36180b9ef3585cb17bf18708
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-02-25 10:36:20 +0300

    first commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,24 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Beste Kocaoglu on 24.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        self.name = simpsonName
+        self.job = simpsonJob
+        self.image = simpsonImage
+    }
+}
